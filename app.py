1649abf3e252229f21f97f52a9ebbbe19eaf6918
kizyofficial <odogwukizito@gmail.com>
2025-08-24 15:26:57 +0100

    added a new file

--- app.py
+++ app.py
@@ -95,4 +95,6 @@ def predict():
 def performance():
 	return render_template('performance.html')   
 if __name__ == "__main__":  
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
